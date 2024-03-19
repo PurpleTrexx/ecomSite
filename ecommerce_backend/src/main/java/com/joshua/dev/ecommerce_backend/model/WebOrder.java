c3a511b2e345d4a9ab89e259105b6594a4f113e6
PurpleTrexx <163590249+PurpleTrexx@users.noreply.github.com>
2024-03-18 21:40:51 -0500

    Add files via upload
    Base files, still need more work on registration

---
+++ ecommerce_backend/src/main/java/com/joshua/dev/ecommerce_backend/model/WebOrder.java
@@ -0,0 +1,58 @@
+package com.joshua.dev.ecommerce_backend.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "web_order")
+public class WebOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private LocalUser user;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<WebOrderQuantities> quantities = new ArrayList<>();
+
+    public List<WebOrderQuantities> getQuantities() {
+        return quantities;
+    }
+
+    public void setQuantities(List<WebOrderQuantities> quantities) {
+        this.quantities = quantities;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public LocalUser getUser() {
+        return user;
+    }
+
+    public void setUser(LocalUser user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
